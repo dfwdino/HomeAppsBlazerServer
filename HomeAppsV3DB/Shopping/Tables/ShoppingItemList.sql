d13c287a64b59a692c0921dbc37c630540505ba2
shane newsom <shaned@pm.me>
2024-02-29 11:20:28 -0600

    Little clean up and update sql proejct

--- HomeAppsV3DB/Shopping/Tables/ShoppingItemList.sql
+++ HomeAppsV3DB/Shopping/Tables/ShoppingItemList.sql
@@ -5,10 +5,13 @@
     [GotItemDate]        DATE CONSTRAINT [DEFAULT_ShoppingItemList_GotItemDate] DEFAULT (getdate()) NULL,
     [NeedDate]           DATE NULL,
     [GotItem]            BIT  CONSTRAINT [DEFAULT_ShoppingItemList_GotItem] DEFAULT ((0)) NOT NULL,
+    [NumberOfItems]      INT  NULL,
     CONSTRAINT [PK_ShoppingItemList] PRIMARY KEY CLUSTERED ([ShoppingItemListID] ASC),
     CONSTRAINT [FK_ShoppingItemList_ShoppingItems] FOREIGN KEY ([ShoppingItemID]) REFERENCES [Shopping].[ShoppingItems] ([ShoppingItemID]),
     CONSTRAINT [FK_ShoppingItemList_ShoppingStores] FOREIGN KEY ([ShoppingStoreID]) REFERENCES [Shopping].[ShoppingStores] ([ShoppingStoreID])
 );
+
+
 
 
 
