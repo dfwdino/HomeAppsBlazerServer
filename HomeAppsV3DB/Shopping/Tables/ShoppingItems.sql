ef7031ccab69c4d5432387ef81f2f587770d0a62
shane newsom <shaned@pm.me>
2024-04-28 17:24:10 -0500

    update DB Layout.

--- HomeAppsV3DB/Shopping/Tables/ShoppingItems.sql
+++ HomeAppsV3DB/Shopping/Tables/ShoppingItems.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [Shopping].[ShoppingItems] (
-    [ShoppingItemID]  INT           IDENTITY (1, 1) NOT NULL,
-    [ItemName]        NVARCHAR (50) NOT NULL,
-    [IsDeleted]       BIT           CONSTRAINT [DEFAULT_ShoppingItems_IsDeleted] DEFAULT ((0)) NOT NULL,
-    [IsGlutenFree]    BIT           CONSTRAINT [DEFAULT_ShoppingItems_IsGlutenFree] DEFAULT ((0)) NOT NULL,
-    [KidsDontLike]    BIT           CONSTRAINT [DEFAULT_ShoppingItems_KidsDontLike] DEFAULT ((0)) NOT NULL,
-    [FreddyDontLike]  BIT           CONSTRAINT [DEFAULT_ShoppingItems_FreddyDontLike] DEFAULT ((0)) NOT NULL,
-    [ElliottDontLike] BIT           CONSTRAINT [DEFAULT_ShoppingItems_ElliottDontLike] DEFAULT ((0)) NOT NULL,
+    [ShoppingItemID]  INT            IDENTITY (1, 1) NOT NULL,
+    [ItemName]        NVARCHAR (50)  NOT NULL,
+    [IsDeleted]       BIT            CONSTRAINT [DEFAULT_ShoppingItems_IsDeleted] DEFAULT ((0)) NOT NULL,
+    [IsGlutenFree]    BIT            CONSTRAINT [DEFAULT_ShoppingItems_IsGlutenFree] DEFAULT ((0)) NOT NULL,
+    [KidsDontLike]    BIT            CONSTRAINT [DEFAULT_ShoppingItems_KidsDontLike] DEFAULT ((0)) NOT NULL,
+    [FreddyDontLike]  BIT            CONSTRAINT [DEFAULT_ShoppingItems_FreddyDontLike] DEFAULT ((0)) NOT NULL,
+    [ElliottDontLike] BIT            CONSTRAINT [DEFAULT_ShoppingItems_ElliottDontLike] DEFAULT ((0)) NOT NULL,
+    [Price]           DECIMAL (5, 2) NULL,
     CONSTRAINT [PK_ShoppingItems] PRIMARY KEY CLUSTERED ([ShoppingItemID] ASC)
 );
+
+
 
