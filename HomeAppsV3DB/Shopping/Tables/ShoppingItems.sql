8a00a15818bb9e2b6e7217c3b40c89e2f74a16aa
shane newsom <shaned@pm.me>
2024-06-22 17:16:41 -0500

    DB update.

--- HomeAppsV3DB/Shopping/Tables/ShoppingItems.sql
+++ HomeAppsV3DB/Shopping/Tables/ShoppingItems.sql
@@ -7,8 +7,12 @@
     [FreddyDontLike]  BIT            CONSTRAINT [DEFAULT_ShoppingItems_FreddyDontLike] DEFAULT ((0)) NOT NULL,
     [ElliottDontLike] BIT            CONSTRAINT [DEFAULT_ShoppingItems_ElliottDontLike] DEFAULT ((0)) NOT NULL,
     [Price]           DECIMAL (5, 2) NULL,
-    CONSTRAINT [PK_ShoppingItems] PRIMARY KEY CLUSTERED ([ShoppingItemID] ASC)
+    [StoreID]         INT            NULL,
+    CONSTRAINT [PK_ShoppingItems] PRIMARY KEY CLUSTERED ([ShoppingItemID] ASC),
+    CONSTRAINT [FK_ShoppingItems_ShoppingStores] FOREIGN KEY ([StoreID]) REFERENCES [Shopping].[ShoppingStores] ([ShoppingStoreID])
 );
+
+
 
 
 
