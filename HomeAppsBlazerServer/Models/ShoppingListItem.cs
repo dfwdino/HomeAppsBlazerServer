edc4ff02f7d7754601115810a9d043e77343fba4
shane newsom <shaned@pm.me>
2024-07-04 23:30:24 -0500

    create model for list item and crated page.

---
+++ HomeAppsBlazerServer/Models/ShoppingListItem.cs
@@ -0,0 +1,20 @@
+﻿namespace HomeAppsBlazerServer.Models
+{
+    public class ShoppingListItem
+    {
+        public int ShoppingItemListID { get; set; }
+        public int ShoppingItemID { get; set; }
+        public int? ShoppingStoreID { get; set; }
+        public DateTime? GotItemDate { get; set; }
+        public DateTime? NeedDate { get; set; }
+        public bool GotItem { get; set; }
+        public int? NumberOfItems { get; set; }
+
+        public string Storename { get; set; }
+        public string ItemName { get; set; }
+
+        // Navigation properties (if needed)
+        // public ShoppingItem ShoppingItem { get; set; }
+        // public ShoppingStore ShoppingStore { get; set; }
+    }
+}
