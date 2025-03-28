88fe09d03a51229e558c4cc9331dd386d2afdab8
shane newsom <shaned@pm.me>
2025-03-28 12:01:21 -0500

    Updated DB.  this will break the current flow in chores for kids.  I am going to rewrite the chore add and view.

---
+++ HomeAppsV3DB/KidsArea/Tables/ChoreAmount.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [KidsArea].[ChoreAmount] (
+    [ID]        INT            IDENTITY (1, 1) NOT NULL,
+    [ChoreID]   INT            NOT NULL,
+    [Amount]    DECIMAL (6, 2) NOT NULL,
+    [IsDeleted] BIT            CONSTRAINT [DF_ChoreAmount_IsDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_ChoreAmount] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_ChoreAmount_Chores] FOREIGN KEY ([ChoreID]) REFERENCES [KidsArea].[Chores] ([ChoreID])
+);
+
